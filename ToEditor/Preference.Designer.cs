da4496206b560f890acc486b0b363d2755074e6e
Kentaro Inomata <kentaro.inomata@gmail.com>
2017-06-27 10:56:13 -0700

    From CodeRepos

---
+++ ToEditor/Preference.Designer.cs
@@ -0,0 +1,128 @@
+﻿#region The MIT License
+/*
+The MIT License
+
+Copyright (c) 2008 matarillo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+namespace Matarillo
+{
+    partial class Preference
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.editorDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "エディタ";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(58, 10);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(197, 19);
+            this.fileTextBox.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(261, 8);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "開く...";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(261, 37);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "決定";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // editorDialog
+            // 
+            this.editorDialog.Filter = "実行ファイル(*.exe)|*.exe|すべてのファイル(*.*)|*.*";
+            // 
+            // Preference
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 68);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Preference";
+            this.Text = "Preference";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.OpenFileDialog editorDialog;
+    }
+}
---
+++ ToEditor/ToEditor.cs
@@ -0,0 +1,218 @@
+﻿#region The MIT License
+/*
+The MIT License
+
+Copyright (c) 2008 matarillo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Matarillo
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("5A6771EB-A51F-47d6-A798-B0FB7D3052A9")]
+    public class ToEditor : IToEditor
+    {
+        private string tempFile;
+        private FileSystemWatcher watcher;
+        private Process proc;
+        private object textChangedHandler;
+        private object exitedHandler;
+
+        public bool Edit(string text)
+        {
+            try
+            {
+                string editorPath = GetEditorPathFromConfig();
+                if (!File.Exists(editorPath))
+                {
+                    return false;
+                }
+
+                tempFile = Path.GetTempFileName();
+                File.WriteAllText(tempFile, text, Encoding.UTF8);
+
+                ProcessStartInfo info = new ProcessStartInfo(editorPath, "\"" + tempFile + "\"");
+                proc = Process.Start(info);
+                proc.Exited += new EventHandler(proc_Exited);
+                proc.EnableRaisingEvents = true;
+
+                watcher = new FileSystemWatcher();
+                watcher.Path = Path.GetDirectoryName(tempFile);
+                watcher.Filter = Path.GetFileName(tempFile);
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+                watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        public void Close()
+        {
+            if (proc != null && !proc.HasExited)
+            {
+                proc.CloseMainWindow();
+                proc.WaitForExit();
+            }
+        }
+
+        public object OnTextChanged
+        {
+            get
+            {
+                return textChangedHandler;
+            }
+            set
+            {
+                textChangedHandler = value;
+            }
+        }
+
+        public object OnExited
+        {
+            get
+            {
+                return exitedHandler;
+            }
+            set
+            {
+                exitedHandler = value;
+            }
+        }
+
+        private void proc_Exited(object sender, EventArgs e)
+        {
+            if (watcher != null)
+            {
+                watcher.EnableRaisingEvents = false;
+                watcher.Dispose();
+                watcher = null;
+            }
+            Exited();
+        }
+
+        private void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                string text = File.ReadAllText(tempFile, Encoding.UTF8);
+                TextChanged(text);
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+
+        private static string GetEditorPathFromConfig()
+        {
+            const string EDITOR_KEY = "EditorPath";
+            string editorPath = null;
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string path = Path.Combine(Path.Combine(appData, "ToEditor"), "app.config");
+            ExeConfigurationFileMap map = new ExeConfigurationFileMap();
+            map.ExeConfigFilename = path;
+            Configuration cfg = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+            if (cfg.HasFile)
+            {
+                KeyValueConfigurationElement ce = cfg.AppSettings.Settings[EDITOR_KEY];
+                editorPath = (ce != null) ? ce.Value : null;
+            }
+            if (!File.Exists(editorPath))
+            {
+                MessageBox.Show("エディタが選択されていません。", "ToEditor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Preference pref = new Preference();
+                DialogResult dr = pref.ShowDialog();
+                editorPath = pref.EditorPath;
+                KeyValueConfigurationElement ce = cfg.AppSettings.Settings[EDITOR_KEY];
+                if (ce == null)
+                {
+                    cfg.AppSettings.Settings.Add(EDITOR_KEY, editorPath);
+                }
+                else
+                {
+                    ce.Value = editorPath;
+                }
+                cfg.Save();
+            }
+            return editorPath;
+        }
+
+        private const BindingFlags CALLBACK
+            = BindingFlags.IgnoreCase
+            | BindingFlags.IgnoreReturn
+            | BindingFlags.Instance
+            | BindingFlags.InvokeMethod
+            | BindingFlags.Public;
+
+        private void Exited()
+        {
+            if (proc != null)
+            {
+                proc.Dispose();
+                proc = null;
+            }
+            if (exitedHandler == null)
+            {
+                return;
+            }
+            Type t = exitedHandler.GetType();
+            try
+            {
+                t.InvokeMember("[DispID=0]", CALLBACK, null, exitedHandler, new object[] { });
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+
+        private void TextChanged(string text)
+        {
+            if (textChangedHandler == null)
+            {
+                return;
+            }
+            Type t = textChangedHandler.GetType();
+            try
+            {
+                t.InvokeMember("[DispID=0]", CALLBACK, null, textChangedHandler, new object[] { text });
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+    }
+}
